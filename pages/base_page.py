b6512c4b7d9224dea0d46995289f25b70814f3fc
Semen <llevchenkosd@gmail.com>
2022-12-13 23:30:00 +0200

    add function 'is_element_present'

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,17 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage:
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(5)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how_find, what_find):
+        try:
+            self.browser.find_element(how_find, what_find)
+        except NoSuchElementException:
+            return False
+        return True
