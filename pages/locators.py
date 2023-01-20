812d97dfebd5b664480d7aed0920df3382e63abb
Semen <llevchenkosd@gmail.com>
2023-01-20 14:12:48 +0200

    add locators and methods for base page

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,20 @@
 from selenium.webdriver.common.by import By
 
+
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
 
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+
+
+class ProductPageLocators:
+    URL = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    ADD_TO_BASKET_BTN = (By.CLASS_NAME, "btn-add-to-basket")
+    ADD_TO_BASKET_MESSAGE = (By.CSS_SELECTOR, "div[id='messages'] strong")
+    PRICE_OF_BASKET_MESSAGE = (By.CSS_SELECTOR, "div[id='messages'] div[class='alertinner '] p")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div[class~='product_main'] h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div[class~='product_main'] p[class='price_color']")
+    PRODUCT_NAME_IN_MESSAGE = (By.CSS_SELECTOR, "div[class='alertinner '] strong")
