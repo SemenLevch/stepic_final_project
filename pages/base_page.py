e638229e5baf57437a764968299617dc2f78b4f2
Semen <llevchenkosd@gmail.com>
2023-03-31 16:47:20 +0300

    add authorized_user assertion

--- pages/base_page.py
+++ pages/base_page.py
@@ -66,3 +66,7 @@ class BasePage:
         link = self.browser.find_element(*BasketPageLocators.BASKET_BUTTON)
         link.click()
 
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
+
